01b9266708ad9fa3f296b7d66bd974b006b1bbfe
Mohamedkhoudi <khoudi.mohamed@yahoo.fr>
2023-03-04 22:00:50 +0100

    My job

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state= {Person:{Fullname:"Med Khoudi",Bio: "Bad front End Developper", imgSrc:"https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png", Profession:"Drug Dealer"}, Show : true, count:0}
+  Handletoggle=()=>this.setState({Show:! this.state.Show});
+ HandleCount =()=>this.setState({Count:this.state.count+1})
+ 
+  componentDidMount(){
+setInterval(this.HandleCount, 1000);
+
+ }
+
+ 
+ 
+  render() {
+    return (
+      <div >
+       <button onClick={this.Handletoggle}>ShowHide</button>
+       
+      {this.state.Show&&<>
+        <h2>
+          {this.state.Person.Fullname}
+        </h2>
+        <h2>
+          {this.state.Person.Bio}
+        </h2>
+        <h2>
+          {this.state.Person.Profession}
+        </h2>
+        <img src={this.state.Person.imgSrc} alt= "goahead"></img>
+        
+        <h2 onTimeUpdate={this.HandleCount}> Timer : {this.state.count}</h2>
+        </>}  
+      </div>
+    )
+  }
 }
-
-export default App;
